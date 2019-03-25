61a85ab1a4f5f1203e5b61f22a25dd150fa397c2
Otto Schuldt <ottoschuldt@gmail.com>
2019-03-26 00:43:40 +0100

    major improvements

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,4 @@ FROM golang
 ADD . /go/src/pdfsigning
 WORKDIR /go/src/pdfsigning
 
-RUN go get github.com/jung-kurt/gofpdf
-
 CMD go run main.go
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"github.com/jung-kurt/gofpdf"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"pdfsigning/utils"
 )
 
@@ -13,6 +16,82 @@ func SignPdfHandler(w http.ResponseWriter, r *http.Request){
 		utils.RenderTemplate(w, "index.gohtml", nil)
 		return
 	}
+
+	// get file from request
+	mf, fh, fileErr := r.FormFile("nf")
+	if fileErr != nil {
+		if fileErr == http.ErrMissingFile {
+			errMsg := "Error: You need to upload a pdf file!"
+			w.WriteHeader(http.StatusForbidden)
+			log.Println(w.Write([]byte(errMsg)))
+			return
+		}
+		log.Println(fileErr)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		err := mf.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// check content type of uploaded file
+	ct := fh.Header.Get("Content-Type")
+
+	// if ct is not pdf, we send an error
+	if ct != "application/pdf" {
+		errMsg := fmt.Sprintf("Error: You need to upload a pdf file! Not a file of type %v", ct)
+		w.WriteHeader(http.StatusForbidden)
+		log.Println(w.Write([]byte(errMsg)))
+		return
+	}
+
+	// create filename of file to be signed
+	filename := "file_to_be_signed.pdf"
+
+	// create paths
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	savePath := filepath.Join(wd, "tmp", filename)
+
+	// we create a new file to copy all the data from the uploaded file
+	nf, err := os.Create(savePath)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		err := nf.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// we rewind the uploaded file
+	_, err = mf.Seek(0, 0)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// we copy the data from the uploaded file to the new created file
+	_, err = io.Copy(nf, mf)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// if all goes well we send a response 200
+	w.WriteHeader(http.StatusOK)
+
 }
 
 func main() {
@@ -23,16 +102,5 @@ func main() {
 		Handler:           mux,
 	}
 	log.Fatal(server.ListenAndServe())
-}
-
-func createPDF()  {
-	pdf := gofpdf.New("P", "mm", "A4", "")
-	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, "Hello, world")
-	err := pdf.OutputFileAndClose("hello.pdf")
-	if err != nil {
-		log.Println(err)
-	}
 }
 
